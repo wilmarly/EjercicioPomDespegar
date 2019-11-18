73a0252b890a210f8e224e6c9bbb25131e629953
wilmar Sanchez <herneysan@gmail.com>
2019-11-18 16:15:11 -0500

    pom reparado

--- src/test/java/tcs/wilmar/pomDespegar/runners/runnerDespegar.java
+++ src/test/java/tcs/wilmar/pomDespegar/runners/runnerDespegar.java
@@ -1,12 +1,14 @@
 package tcs.wilmar.pomDespegar.runners;
 
 import org.junit.runner.RunWith;
-
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features ="src/test/resources/features/validarPrecio.feature")
+@CucumberOptions(features ="src/test/resources/features/validarPrecio.feature", glue = "tcs.wilmar.pomDespegar.steps")
 public class runnerDespegar {
 
 }
--- src/test/java/tcs/wilmar/pomDespegar/steps/buscarYvalidarVuelo_steps.java
+++ src/test/java/tcs/wilmar/pomDespegar/steps/buscarYvalidarVuelo_steps.java
@@ -1,5 +1,13 @@
 package tcs.wilmar.pomDespegar.steps;
 
-public class buscarYvalidarVuelo_steps {
+import net.thucydides.core.annotations.Step;
+import tcs.wilmar.pomDespegar.pageObject.buscarYvalidarVuelo_pageObject;
 
+public class buscarYvalidarVuelo_steps {
+	buscarYvalidarVuelo_pageObject PgObj;
+
+	@Step
+	public void select_origen() {
+		PgObj.click_en_campo_origen();
+	}
 }
